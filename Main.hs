816f8b847c8385f881b243ff215a0d7515bdca87
eddyz87 <eddyz87@gmail.com>
2015-07-20 01:29:48 +0300

    Added parseString utility function

--- Main.hs
+++ Main.hs
@@ -4,13 +4,25 @@ import Debug.Trace(traceIO)
 import Lexer
 import Parser
 import Tokens
+import Ast
 import System.Environment(getArgs)
 
 main :: IO ()
 main = do
   args <- getArgs
-  traceIO $ show $ runAlex (foldr (++) "" args) tokenize
+  putStrLn $ show $ parseString (foldr (++) "" args)
   return ()
+
+parseString :: String -> Either String [Dec]
+parseString str = 
+  let tokens = runAlex str tokenize in
+   case tokens of
+   Right lst -> Right $ parse $ map token2TokenKind lst
+   Left err -> Left $ "Lexer error: " ++ err
+
+
+token2TokenKind :: Token -> TokenKind
+token2TokenKind (Token _ k) = k
 
 tokenize :: Alex [Token]
 tokenize = loop []
